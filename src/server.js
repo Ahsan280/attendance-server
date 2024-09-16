1104e085fa43e67179cc7b135fa5d9e5c70215d4
Ahsan280 <ahsanrehman281@gmail.com>
2024-09-16 11:08:19 +0500

    Pinging the server to keep it alive

--- src/server.js
+++ src/server.js
@@ -39,3 +39,29 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/attendance", attendanceRouter);
 app.use("/api/v1/time", timeRouter);
 app.use("/api/v1/application", applicationRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
+
+// Keep-alive ping code
+const url = process.env.PRODUCTION_URL;
+const interval = 60000; // Ping every 60 seconds
+
+function keepAlive() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(
+        `Pinged at ${new Date().toISOString()}: Status Code ${response.status}`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error pinging at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(keepAlive, interval);
