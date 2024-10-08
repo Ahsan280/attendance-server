571200f9b24d30a346e4648f388758b223c66481
Ahsan280 <ahsanrehman281@gmail.com>
2024-10-08 15:15:57 +0500

    connect to database

--- src/db/index.js
+++ src/db/index.js
@@ -1,22 +1,24 @@
-import { Sequelize } from "sequelize";
+import mysql from "mysql";
 import dotenv from "dotenv";
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
+dotenv.config({
+  path: "./.env",
+});
 
-  {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dialect: "mysql",
+const connection = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  connectTimeout: 10000,
+  port: process.env.DATABASE_PORT,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting: " + err.stack);
+    return;
   }
-);
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
-export default sequelize;
+  console.log("Connected as id " + connection.threadId);
+});
+
+export default connection;
